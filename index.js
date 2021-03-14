a948214cf45856e70022efa82a03f774ea236093
unknown <bnik.cool@gmail.com>
2021-03-14 21:25:44 +0200

    migrated to pug + url issue after submitting the form

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const {loadLaptop} = require('./dataLoader')
 //server
 const app = express()
 app.use(express.json())
+app.set('views', './views')
+app.set('view engine', 'pug')
 app.listen(3000, () => console.log('Server started.'))
 
 //db
@@ -12,18 +14,18 @@ mongoose.set('useNewUrlParser', true)
 mongoose.set('useUnifiedTopology', true);
 mongoose.connect('mongodb://localhost/pufferfish', () => console.log('Connected to DB'))
 
-
 // loadLaptop('test.json')
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+    res.render('index', {
+        title: 'Pufferfish',
+    })
 })
 
-app.get('/result', (req, res) => {
-    res.send('Ok r')
-})
-
-app.get('/api', (req, res) => {   
-    console.log(req.query)
-    res.redirect('/result')
+app.get('/result', (req, res) => {   
+    console.log(req.body)
+    res.render('result', {
+        title: 'Pufferfish: Laptop Result',
+        score: req.query
+    })
 })
