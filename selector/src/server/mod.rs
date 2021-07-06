d8a787df4d8b6392162fcb287ea490bafa4c1cb4
Mader Noob <madernoob@protonmail.com>
2021-07-06 23:45:20 +0300

    now sending a failure response to the client on error

--- selector/src/server/mod.rs
+++ selector/src/server/mod.rs
@@ -1,9 +1,9 @@
 use std::{collections::HashMap, net::{TcpListener, TcpStream}, time::Instant};
 
-use crate::errors::*;
+use crate::{errors::*, selection::SelectedLaptopInfo};
 use crate::selection;
 use diesel::PgConnection;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 const SERVER_ENDPOINT: &str = "127.0.0.1:4741";
 
@@ -12,6 +12,12 @@ struct SelectionRequest {
     amount: usize,
     max_price: Option<f32>,
     category_scores: HashMap<String, f32>,
+}
+
+#[derive(Debug, Serialize)]
+struct SelectionResponse{
+    success: bool,
+    laptops: Option<Vec<SelectedLaptopInfo>>,
 }
 
 pub fn start_server(db_connection: &PgConnection) -> Result<()> {
@@ -28,9 +34,9 @@ pub fn start_server(db_connection: &PgConnection) -> Result<()> {
     Ok(())
 }
 
-fn handle_client(stream: TcpStream, db_connection: &PgConnection) -> Result<()> {
+fn try_handle_client(stream: &TcpStream, db_connection: &PgConnection) -> Result<()> {
     // receive the selection request from the client
-    let request: SelectionRequest = serde_json::from_reader(&stream)
+    let request: SelectionRequest = serde_json::from_reader(stream)
         .into_selector_result(SelectorErrorKind::FailedToReceiveRequestFromClient)?;
 
     // perform the selection and measure the elapsed time
@@ -44,9 +50,29 @@ fn handle_client(stream: TcpStream, db_connection: &PgConnection) -> Result<()> 
     let elapsed = Instant::now() - start;
 
     // send the response to the client
-    serde_json::to_writer(&stream, selection_results.as_slice())
+    let response = SelectionResponse{
+        success: true,
+        laptops: Some(selection_results),
+    };
+    serde_json::to_writer(stream, &response)
         .into_selector_result(SelectorErrorKind::FailedToSendResponseToClient)?;
 
     println!("selection elapsed time: {:?}",elapsed);
     Ok(())
+}
+
+fn handle_client(stream: TcpStream, db_connection: &PgConnection)->Result<()>{
+    match try_handle_client(&stream, db_connection){
+        Ok(()) => Ok(()),
+        Err(e)=>{
+            // in case of an error, send a failure response to the client and return the error
+            let response = SelectionResponse{
+                success: false,
+                laptops: None,
+            };
+            serde_json::to_writer(stream, &response)
+                .into_selector_result(SelectorErrorKind::FailedToSendResponseToClient)?;
+            Err(e)
+        }
+    }
 }
