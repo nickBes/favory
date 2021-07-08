2255d8d75ec9d1ef3025935c83cce02699632864
Mader Noob <madernoob@protonmail.com>
2021-07-08 20:25:08 +0300

    the results page now performs the selection and returns its results

--- webapp/pages/results.tsx
+++ webapp/pages/results.tsx
@@ -3,30 +3,171 @@ import Error from '../components/error/error'
 import { GetServerSideProps } from 'next'
 import getRawBody from 'raw-body'
 import qs from 'querystring'
+import { SelectedLaptopInfo, SelectionRequest, select } from '../selector'
 
-interface ResultProps {
-    body : Object
+
+type SelectionResultSuccess = {
+    status: "success",
+    laptops: SelectedLaptopInfo[]
 }
 
-const Results : React.FC<{resultProps : ResultProps}> = ({resultProps}) => { 
-    if (JSON.stringify(resultProps.body) == JSON.stringify({})) {
+type SelectionResultInvalidField = {
+    status: "invalidField",
+    fieldName: string
+}
+
+type SelectionResultFailure = {
+    status: "failure",
+    error: string,
+}
+
+type SelectionResult =
+    | SelectionResultSuccess
+    | SelectionResultInvalidField
+    | SelectionResultFailure
+
+// hardcoding the categories is a temporary solution to allow for checking,
+// the categories should be fetched in _app.tsx using getStaticServerProps
+const AVAILABLE_CATEGORIES = new Set([
+    "dev", "study", "design", "gaming"
+])
+
+const Results: React.FC<{ selectionResult: SelectionResult }> = ({ selectionResult }) => {
+    console.log('selection result:',selectionResult)
+    if(selectionResult.status == "success"){
         return (
-            <Error/>
+            <div>{JSON.stringify(selectionResult.laptops)}</div>
+        )
+    }else if(selectionResult.status == "invalidField"){
+        // somehow include information about the invalid field (selectionResult.fieldName)
+        return (
+            <Error></Error>
+        )
+    }else {
+        // somehow include information about the error (selectionResult.error)
+        return (
+            <Error></Error>
         )
     }
-    return (
-        <div>{JSON.stringify(resultProps.body)}</div>
-    )
 }
 
 export default Results
 
-export const getServerSideProps : GetServerSideProps = async ctx => {
-    let resultProps : ResultProps = {
-        body: {}
+type SelectionRequestExtractionResultSuccess = {
+    success: true,
+    selectionRequest: SelectionRequest,
+}
+
+type SelectionRequestExtractionResultInvalidField = {
+    success: false,
+    fieldName: string,
+}
+
+type SelectionRequestExtractionResult =
+    | SelectionRequestExtractionResultInvalidField
+    | SelectionRequestExtractionResultSuccess
+
+// extracts the selection request from the query
+function extractSelectionRequestFromQuery(query: qs.ParsedUrlQuery): SelectionRequestExtractionResult {
+    // first parse all the fields into numbers
+    let parsedFields: { [fieldName: string]: number } = {}
+    for (const fieldName in query) {
+        let value = query[fieldName]
+        // we expect each field to be a string
+        if (typeof value != 'string') {
+            return {
+                success: false,
+                fieldName
+            }
+        }
+        let parsedValue = Number.parseFloat(value);
+        // if the parsedValue is NaN, it means that we failed to parse it
+        if (isNaN(parsedValue)) {
+            return {
+                success: false,
+                fieldName
+            }
+        }
+
+        parsedFields[fieldName] = parsedValue;
     }
+
+    // extract the amount
+    let amount = parsedFields.amount;
+    // we must delete the property since after extracting the pre-known
+    // properties (the properties that we know the name of in advance),
+    // we iterate over the rest of the properties, which should
+    // represents the category scores.
+    delete parsedFields.amount;
+
+    // extract the max price, note that it is an optional field
+    let maxPrice: number | undefined;
+    if ('maxPrice' in parsedFields) {
+        maxPrice = parsedFields.maxPrice
+        // we must delete the property since after extracting the pre-known
+        // properties, we iterate over the rest of the properties, which should
+        // represents the category scores
+        delete query.maxPrice;
+    }
+
+    // the rest of the field should represent the category scores,
+    // make sure all field names correspond to category names
+    for (const fieldName in parsedFields) {
+        if (!AVAILABLE_CATEGORIES.has(fieldName)) {
+            return {
+                success: false,
+                fieldName
+            }
+        }
+    }
+
+    return {
+        success: true,
+        selectionRequest: {
+            amount, maxPrice, categoryScores: parsedFields,
+        }
+    }
+}
+
+// extracts the selection request from the query, performs the selection,
+// and returns the result
+async function performRequestedSelection(query: qs.ParsedUrlQuery): Promise<SelectionResult> {
+    let selectionRequestExtractionResult = extractSelectionRequestFromQuery(query)
+    if (selectionRequestExtractionResult.success) {
+        try {
+            let laptops = await select(selectionRequestExtractionResult.selectionRequest)
+            return {
+                status: "success",
+                laptops
+            }
+        } catch (e) {
+            return {
+                status: "failure",
+                error: (e as Error).message
+            }
+        }
+    } else {
+        return {
+            status: "invalidField",
+            fieldName: selectionRequestExtractionResult.fieldName,
+        }
+    }
+}
+
+export const getServerSideProps: GetServerSideProps = async ctx => {
+    let result: SelectionResult;
     if (ctx.req.method == 'POST') {
-        resultProps.body = qs.parse(await getRawBody(ctx.req, {encoding: 'utf-8'}))
+        let query = qs.parse(await getRawBody(ctx.req, { encoding: 'utf-8' }))
+        result = await performRequestedSelection(query);
+    } else {
+        result = {
+            status: "failure",
+            error: 'only POST is allowed'
+        }
     }
-    return {props: {resultProps: resultProps}}
+    return {
+        props: {
+            selectionResult: result
+        }
+    }
 }
--- webapp/selector.ts
+++ webapp/selector.ts
@@ -1,42 +1,59 @@
 import * as net from 'net'
 import PromiseSocket from 'promise-socket'
+import {Mutex} from 'async-mutex';
 
 const SELECTOR_SERVER_PORT = 4741
 
-type SelectionRequest = {
+export type SelectionRequest = {
     amount: number,
-    max_price?: number,
-    category_scores: {
+    maxPrice?: number,
+    categoryScores: {
         [category_name: string]: number,
     }
 }
 
-type SelectedLaptopInfo = {
+export type SelectedLaptopInfo = {
     name: string,
 }
 
 type SelectionResponse = {
     success: boolean,
-    laptops: [SelectedLaptopInfo] | null,
+    laptops: SelectedLaptopInfo[] | null,
 }
 
 const socket = new PromiseSocket(new net.Socket());
+const mutex = new Mutex();
 
-async function connectToSelector() {
-    socket.setEncoding('utf8')
-    await socket.connect(SELECTOR_SERVER_PORT, "127.0.0.1");
-}
+// this funciton is an IIF (immediately invoked function) since next js doesn't support running
+// code on startup
+(async function connectToSelector() {
+    // we lock the mutex here to make sure no one can perform selection until we are connected
+    // to the selector
+    await mutex.runExclusive(async ()=>{
+        socket.setEncoding('utf8')
+        await socket.connect(SELECTOR_SERVER_PORT, "127.0.0.1");
+    })
+})()
 
-async function select(request: SelectionRequest): Promise<[SelectedLaptopInfo]> {
-    await socket.write(JSON.stringify(request))
-    const raw_response = await socket.readAll();
-    let response: SelectionResponse;
-    if (typeof raw_response == 'string'){
-        response = JSON.parse(raw_response)
-    } else if(raw_response instanceof Buffer){
-        response = JSON.parse(raw_response.toString())
-    }else{
-        throw new Error('failed to perform selection: the selector did not response')
+export async function select(request: SelectionRequest): Promise<SelectedLaptopInfo[]> {
+    let response: SelectionResponse|undefined;
+    await mutex.runExclusive(async ()=>{
+        await socket.write(JSON.stringify(request))
+        const raw_response = await socket.readAll();
+        if (typeof raw_response == 'string'){
+            response = JSON.parse(raw_response)
+        } else if(raw_response instanceof Buffer){
+            response = JSON.parse(raw_response.toString())
+        }else{
+            throw new Error('failed to perform selection: the selector did not response')
+        }
+    })
+    // this should never happed, since the callback in mutex.runExclusive should
+    // either set the response and throw the exceptions, and in both cases this 
+    // should never happen, but this check is added to resolve the type errors
+    // of using response without checking if its undefined.
+    if(response === undefined){
+        throw new Error('failed to perform selection: an unknown error has occured')
     }
     if(!response.success || response.laptops === null){
         throw new Error('failed to perform selection: the selector returned a failure response')
