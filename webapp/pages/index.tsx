3e30ac5f44f055dc579baff4b6349c6b6a507a91
Nick <>
2021-07-20 01:48:08 +0300

    horizontal scrolling has been fixed as per #30, although a better scrolling to objects on click hasn't been implemented yet

--- webapp/pages/index.tsx
+++ webapp/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useRef} from 'react'
+import React, { useState, useRef } from 'react'
 import { GetStaticProps } from 'next'
 import Navbar from '../components/navbar/navbar'
 import Form from '../components/form/form'
@@ -8,59 +8,65 @@ import SearchBar from '../components/form/searchbar'
 import Scrollable from '../components/navigation/scrollable'
 
 interface HomeProps {
-  categories : Array<string>
+  categories: Array<string>
 }
 
-const Home : React.FC <{homeProps : HomeProps}> = ({homeProps}) => {
+const Home: React.FC<{ homeProps: HomeProps }> = ({ homeProps }) => {
   const [tags, setTags] = useState(new Set<string>())
   const formRef = useRef<null | HTMLElement>(null)
   const ratingRef = useRef<null | HTMLElement>(null)
+  const tagRef = useRef<null | HTMLElement>(null)
 
-  const removeTag = (tag : string) => {
+  const removeTag = (tag: string) => {
     setTags(new Set(Array.from(tags).filter(value => value != tag)))
   }
 
-  const addTag = (suggestion : string) => {
+  const addTag = (suggestion: string) => {
     setTags(new Set([...tags, suggestion]))
   }
 
   return (
-    <>  
-      {/* sections have temporary class names in order to distinguish between them */}
-      <section className='p'>
+    <>
+      <section>
         <Navbar></Navbar>
         <div className='main'>
           <h1>Hey, what's going on guys it's me jermey.</h1>
           <p>Did you know that kndred was actually jin?</p>
           <button onClick={() => {
-            formRef.current?.scrollIntoView({behavior: 'smooth'})
+            formRef.current?.scrollIntoView({ behavior: 'smooth' })
           }}>Scroll to form.</button>
           <img src="https://wallpaperaccess.com/full/1369012.jpg"></img>
         </div>
       </section>
-      <section ref={formRef} className='s'>
-        <Form formAttr={{action: "./results", method: 'post'}}>
+      <section ref={formRef} className='gray'>
+        <Form formAttr={{ action: "./results", method: 'post' }}>
           <Scrollable direction='horizontal'>
-            <section>
-              <Tags tagProps={{tags: tags, onClick:removeTag}}></Tags>
-              <SearchBar searchBarProps={{suggestions: new Set(homeProps.categories), onClick:addTag, maxListSize:5}}></SearchBar>
-              <a onClick={() => {
-                ratingRef.current?.scrollIntoView({behavior: 'smooth', block:'center', inline:'nearest'})
-              }}>Next</a>
+            <section ref={tagRef}>
+              <div>
+                <Tags tagProps={{ tags: tags, onClick: removeTag }}></Tags>
+                <SearchBar searchBarProps={{ suggestions: new Set(homeProps.categories), onClick: addTag, maxListSize: 5 }}></SearchBar>
+                <a onClick={() => {
+                  ratingRef.current?.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'nearest' })
+                }}>Next</a>
+              </div>
             </section>
             <section ref={ratingRef}>
-              {Array.from(tags).map(tag => {
-                return (
-                  <Slider key={tag} sliderProps={{inputName: tag, max: 100, min: 0, defaultValue: 50}}></Slider>
-                )
-              })}
-              <input type="submit"/>
+              <div>
+                {Array.from(tags).map(tag => {
+                  return (
+                    <Slider key={tag} sliderProps={{ inputName: tag, max: 100, min: 0, defaultValue: 50 }}></Slider>
+                  )
+                })}
+                <br />
+                <a onClick={() => {
+                  tagRef.current?.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'nearest' })
+                }}>Prev</a>
+                <br />
+                <input type="submit" />
+              </div>
             </section>
           </Scrollable>
         </Form>
-      </section>
-      <section className='p'>
-        <h1>Hey, what's going on guys it's me jermey.</h1>
       </section>
     </>
   )
@@ -68,11 +74,11 @@ const Home : React.FC <{homeProps : HomeProps}> = ({homeProps}) => {
 
 export default Home
 
-export const getStaticProps : GetStaticProps = async ctx => {
-    let homeProps : HomeProps = {
-      categories: new Array<string>()
-    }
-    // get categories from database
-    homeProps.categories = ['gaming', 'dev', 'design', 'study']
-    return {props: {homeProps: homeProps}}
+export const getStaticProps: GetStaticProps = async ctx => {
+  let homeProps: HomeProps = {
+    categories: new Array<string>()
+  }
+  // get categories from database
+  homeProps.categories = ['gaming', 'dev', 'design', 'study']
+  return { props: { homeProps: homeProps } }
 }
