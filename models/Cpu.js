d83c3d2e667cee0ecfdfbed0e6c76357a3f3f41f
Nick <>
2021-08-02 01:21:48 +0300

    Removed pre-migration code as per #49, and added readme.md to redirect people to the relevant docs and save algorithm explenations

--- models/Cpu.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-
-const cpuSchema = new mongoose.Schema({
-	name: String
-})
-
-module.exports = mongoose.model('Cpu', cpuSchema)
--- models/Gpu.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-
-const gpuSchema = new mongoose.Schema({
-	name: String
-})
-
-module.exports = mongoose.model('Gpu', gpuSchema)
