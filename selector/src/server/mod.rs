be47c7a5c27738dc0fd239c9423e40d19007e9a8
Mader Noob <madernoob@protonmail.com>
2021-08-01 22:36:29 +0300

    selector is now object oriented

--- selector/src/server/mod.rs
+++ selector/src/server/mod.rs
@@ -13,6 +13,58 @@ use serde::{Deserialize, Serialize};
 const SERVER_ENDPOINT: &str = "127.0.0.1:4741";
 const BUFFER_SIZE: usize = 16384;
 
+struct ClientHandler<'buf, 'conn>{
+    stream: TcpStream,
+    buffer: &'buf mut [u8],
+    db_connection: &'conn PgConnection,
+}
+impl<'buf, 'conn> ClientHandler<'buf, 'conn>{
+    fn handle_client(&mut self)->Result<()>{
+        loop {
+            if let Err(e) = self.handle_client_request() {
+                println!("error while handling client: {:?}", e);
+
+                // in case the error that occured is a tcp stream error,
+                // the tcp stream has broke, so we should stop handling the client
+                // and move on to the next client
+                if e.kind == SelectorErrorKind::TcpStreamError {
+                    return Err(e);
+                }
+
+                // in case of an error that did not break the connection, send a failure response to the client
+                let response:SelectorResponse<()> = SelectorResponse {
+                    success: false,
+                    content: None,
+                };
+                let serialized_response = serde_json::to_vec(&response)
+                    .into_selector_result(SelectorErrorKind::FailedToSerializeResponse)?;
+                self.stream
+                    .write_all(&serialized_response)
+                    .into_selector_result(SelectorErrorKind::TcpStreamError)?;
+            }
+        }
+    }
+    fn handle_client_request(&mut self)->Result<()>{
+        // receive and deserialize the selection request from the client
+        let length = self.stream
+            .read(self.buffer)
+            .into_selector_result(SelectorErrorKind::TcpStreamError)?;
+        // a length of 0 means that the stream has closed
+        if length == 0 {
+            return Err(SelectorErrorKind::TcpStreamError.into_empty_selector_error());
+        }
+        let request: SelectorRequest = serde_json::from_slice(&self.buffer[..length])
+            .into_selector_result(SelectorErrorKind::FailedToDeserializeClientRequest)?;
+
+        // serialize and send the response to the client
+        let serialized_response = request.handle_request_and_serialize_response(self.db_connection)?;
+        self.stream
+            .write_all(&serialized_response)
+            .into_selector_result(SelectorErrorKind::TcpStreamError)?;
+        Ok(())
+    }
+}
+
 #[derive(Debug, Deserialize)]
 #[serde(tag = "type", content = "parameters")]
 enum SelectorRequest {
@@ -76,62 +128,17 @@ pub fn start_server(db_connection: &PgConnection) -> Result<()> {
         // possible stream might contain an error, so we must first check for that case
         let stream =
             possible_stream.into_selector_result(SelectorErrorKind::FailedToAcceptClient)?;
-        if let Err(e) = handle_client(stream, &mut buffer, db_connection) {
+        
+        // create the handler struct containing all information required for handling the client
+        let mut client_handler = ClientHandler{
+            stream,
+            buffer: &mut buffer,
+            db_connection
+        };
+
+        if let Err(e) = client_handler.handle_client() {
             eprintln!("error while handling client: {:?}", e);
         }
     }
     Ok(())
-}
-
-fn handle_clients_request(
-    stream: &mut TcpStream,
-    buffer: &mut [u8],
-    db_connection: &PgConnection,
-) -> Result<()> {
-    // receive and deserialize the selection request from the client
-    let length = stream
-        .read(buffer)
-        .into_selector_result(SelectorErrorKind::TcpStreamError)?;
-    // a length of 0 means that the stream has closed
-    if length == 0 {
-        return Err(SelectorErrorKind::TcpStreamError.into_empty_selector_error());
-    }
-    let request: SelectorRequest = serde_json::from_slice(&buffer[..length])
-        .into_selector_result(SelectorErrorKind::FailedToDeserializeClientRequest)?;
-
-    // serialize and send the response to the client
-    let serialized_response = request.handle_request_and_serialize_response(db_connection)?;
-    stream
-        .write_all(&serialized_response)
-        .into_selector_result(SelectorErrorKind::TcpStreamError)?;
-    Ok(())
-}
-
-fn handle_client(
-    mut stream: TcpStream,
-    buffer: &mut [u8],
-    db_connection: &PgConnection,
-) -> Result<()> {
-    loop {
-        if let Err(e) = handle_clients_request(&mut stream, buffer, db_connection) {
-            // in case the error that occured is a tcp stream error,
-            // the tcp stream has broke, so we should stop handling the client
-            // and move on to the next client
-            if e.kind == SelectorErrorKind::TcpStreamError {
-                return Err(e);
-            }
-            println!("error while handling client: {:?}", e);
-
-            // in case of an error, send a failure response to the client
-            let response:SelectorResponse<()> = SelectorResponse {
-                success: false,
-                content: None,
-            };
-            let serialized_response = serde_json::to_vec(&response)
-                .into_selector_result(SelectorErrorKind::FailedToSerializeResponse)?;
-            stream
-                .write_all(&serialized_response)
-                .into_selector_result(SelectorErrorKind::TcpStreamError)?;
-        }
-    }
 }
