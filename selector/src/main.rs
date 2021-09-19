20acb62738110b47000da5164f5778df37eccb9f
Mader Noob <madernoob@protonmail.com>
2021-09-20 01:21:52 +0300

    added logging to the selector server

--- selector/src/main.rs
+++ selector/src/main.rs
@@ -1,4 +1,8 @@
+use std::fs::OpenOptions;
+
 use diesel::PgConnection;
+use log::LevelFilter;
+use simplelog::{Config, SimpleLogger, WriteLogger};
 
 #[macro_use]
 extern crate diesel;
@@ -11,6 +15,16 @@ mod server;
 pub struct SelectorDBConnection(PgConnection);
 
 fn main() {
+    // initialize logger
+    if cfg!(debug_assertions) {
+        // in debug mode print logs to stdout
+        SimpleLogger::init(LevelFilter::max(), Config::default()).expect("failed to initialize logger");
+    } else{
+        // in release mode print logs to file
+        let config_file = OpenOptions::new().write(true).create(true).open("log").expect("failed to open log file");
+        WriteLogger::init(LevelFilter::Warn, Config::default(), config_file).expect("failed to initialize logger");
+    }
+
     let db_connection = SelectorDBConnection(db_access::get_db_connection());
 
     if let Err(e) = server::start_server(&db_connection){
