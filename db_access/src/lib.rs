1407817829f21830d5091e3b04e7b4fad0aad2c7
nickBes <>
2021-09-21 18:33:14 +0300

    hopefully removed merge conflict comment

--- db_access/src/lib.rs
+++ db_access/src/lib.rs
@@ -8,15 +8,11 @@ pub mod error_types_generator;
 use diesel::{Connection, PgConnection};
 
 pub fn get_db_connection()->PgConnection{
-<<<<<<< Updated upstream
-    let db_url = if cfg!(debug_assertions){
-        "postgres://postgres:favory@localhost/favory".into()
-    }else{
-        std::env::var("DATABASE_URL").expect("the DATABASE_URL environment variable must be set")
-    };
-    PgConnection::establish(&db_url).unwrap()
+        let db_url = if cfg!(debug_assertions){
+            "postgres://postgres:favory@localhost/favory".into()
+        }else{
+            std::env::var("DATABASE_URL").expect("the DATABASE_URL environment variable must be set")
+        };
+        PgConnection::establish(&db_url).unwrap()
+    }
 }
-=======
-    PgConnection::establish("postgres://postgres:favory@localhost/favory").unwrap()
-}
->>>>>>> Stashed changes
