6a2f907900de4d1c821ecaf60e2d3fc06d670f7e
nickBes <>
2021-09-22 02:20:08 +0300

    added tooltip for multislider as per #76

--- webapp/components/form/multi_slider.tsx
+++ webapp/components/form/multi_slider.tsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import 'rc-slider/assets/index.css'
 import { Range } from 'rc-slider'
+import Tooltip from './tooltip'
 
 interface MultiSliderProps {
     tags: string[],
@@ -16,7 +17,8 @@ interface CategoryAndSliderValues {
 }
 
 const MultiSlider : React.FC<MultiSliderProps> = ({tags, min, max}) => {
-
+    const rangeRef = useRef<HTMLDivElement>(null)
+    const [rangeWidth, setRangeWidth] = useState(0)
     // use state is only called once, to update the categoryAndSliderValues so we need to use useEffect
     // that is called when tags are changed
     const [categoryAndSliderValues, setcategoryAndSliderValues] = useState<CategoryAndSliderValues>({categories: {}, values: []})
@@ -36,6 +38,10 @@ const MultiSlider : React.FC<MultiSliderProps> = ({tags, min, max}) => {
         }
         setcategoryAndSliderValues(generateDefaultValues())
     }, [tags, min, max])
+
+    useEffect(() => {
+        setRangeWidth(rangeRef.current?.clientWidth ?? 0)
+    }, [rangeRef])
 
     const handleSliderChange = (values : number[]) => {
         setcategoryAndSliderValues(prev => {
@@ -64,14 +70,36 @@ const MultiSlider : React.FC<MultiSliderProps> = ({tags, min, max}) => {
         return inputList
     }
 
+    const createTooltipFromCategoryAndSliderValues = (categoryAndSliderValues : CategoryAndSliderValues, max : number, min: number, rangeWidth: number) => {
+        const values = categoryAndSliderValues.values
+        let tooltipList = []
+        let top = true
+        const tags = Object.keys(categoryAndSliderValues.categories)
+        for (let i = 1; i < values.length; i++) {
+            tooltipList.push(<Tooltip 
+                                    content={`${tags[i - 1]} ${Math.floor(100 * (values[i] - values[i - 1])/(max-min))}%`} 
+                                    distanceFromLeft={(values[i] + values[i - 1])/((max - min) * 2) * rangeWidth}
+                                    position={top ? 'top' : 'bottom'}
+                                    key={i}>
+                            </Tooltip>)
+            top = !top
+        }
+        return tooltipList
+    }
+
     return (
         <>
-            <p>{JSON.stringify(categoryAndSliderValues)}</p>
-            <Range onChange={handleSliderChange}
-                    value={categoryAndSliderValues.values.slice(1, categoryAndSliderValues.values.length - 1)} 
-                    allowCross={false}
-                    pushable={true}
-            />
+            <div ref={rangeRef} style={{
+                            maxWidth: 700,
+                            position: 'relative'
+                        }}>
+                <Range onChange={handleSliderChange}
+                        value={categoryAndSliderValues.values.slice(1, categoryAndSliderValues.values.length - 1)} 
+                        allowCross={false}
+                        pushable={true}
+                />
+                {createTooltipFromCategoryAndSliderValues(categoryAndSliderValues, max, min, rangeWidth)}
+            </div>
             {/* Range doesn't create ibputs, so we need to create them ourselves as
                 the values are passed with a <form> */}
             {createInputsFromCategoryValues(categoryAndSliderValues.categories)}
