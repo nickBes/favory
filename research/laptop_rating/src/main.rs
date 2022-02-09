2094af0265a17b6d9829207fa1d41dc6b77d56d9
nickBes <58567842+nickBes@users.noreply.github.com>
2022-02-09 21:39:51 +0200

    now can calculate the scores in a graph

--- research/laptop_rating/src/main.rs
+++ research/laptop_rating/src/main.rs
@@ -2,10 +2,14 @@ use std::collections::HashMap;
 use std::vec::Vec;
 use petgraph::graph::Graph;
 use petgraph::dot::Dot;
-use petgraph::Directed;
+use petgraph::{Directed, Outgoing, Incoming};
+use petgraph::prelude::NodeIndex;
+use petgraph::visit::EdgeRef;
 use std::cmp::Ordering;
 
 type Pair = (usize, usize);
+type TestGraph = Graph<char, isize, Directed, usize>;
+type ScoreMap = HashMap<NodeIndex<usize>, isize>;
 
 #[derive(Copy, Clone)]
 struct WeightedPair {
@@ -19,8 +23,31 @@ fn reverse (pair : &Pair) -> Pair {
     (b, a)
 }
 
+fn calc_node_score (graph: &TestGraph, scores: &mut ScoreMap, index: &NodeIndex<usize>)  -> isize{
+    let mut sum : isize = 1;
+    for edge in graph.edges_directed(*index, Incoming) {
+        let parent_index = edge.source();
+
+        match scores.get(&parent_index) {
+            Some(score) => sum += *score * edge.weight(),
+            _ => {
+                sum += calc_node_score(graph, scores, &parent_index) * edge.weight();
+            }
+        }
+    }
+    scores.insert(*index, sum);
+    sum
+}
+
+fn calc_scores (graph: &TestGraph, scores: &mut ScoreMap, source_index : &NodeIndex<usize>) {
+    for edge in graph.edges_directed(*source_index, Outgoing) {
+        calc_node_score(graph, scores, &edge.target());
+        calc_scores(graph, scores, &edge.target());
+    }
+}
+
 fn is_cycle (pair : &WeightedPair, pairs : &Vec<WeightedPair>) -> bool {
-    // get pssible cases for cycles and check them
+    // get possible cases for cycles and check them
     if let Some(_) = pairs
                         .into_iter()
                         .filter(|val| val.follower == pair.followed)
@@ -68,13 +95,9 @@ fn add_pair (pair : &Pair, pairs : &mut HashMap<Pair, isize>) {
 fn main() {
     let items = vec!['a', 'b', 'c', 'd'];
     let ballots = vec![
-        vec![0, 3, 2],
-        vec![0, 1, 2, 3],
-        vec![3, 0, 2],
-        vec![2, 1],
-        vec![2, 1],
-        vec![3,0],
-        vec![3,0]
+        vec![0, 2, 3],
+        vec![1, 3],
+        vec![2,1]
     ];
     let mut pairs : HashMap<Pair, isize> = HashMap::new();
     for ballot in &ballots {
@@ -124,14 +147,31 @@ fn main() {
     }
 
     // create a graph to format it into a dot format
-    let mut graph : Graph<char, isize, Directed, usize> = Graph::from_edges(filtered_pairs
-                                                                            .iter()
-                                                                            .map(|val| (val.follower, val.followed, val.weight)));
-    // sets the labels for the graph
+    let mut graph : TestGraph = Graph::from_edges(filtered_pairs
+                                                            .iter()
+                                                            .map(|val| (val.follower, val.followed, val.weight)));
+
+    let mut source_indexes  = Vec::new();
     for index in graph.node_indices() {
+        // sets the labels for the graph
         if let Some(weight) = graph.node_weight_mut(index) {
             *weight = items[index.index()];
         }
+
+        // finds the source of the graph, if nobody is
+        // following the node in the current index
+        if graph.neighbors_directed(index, Incoming).count() == 0 {
+            source_indexes.push(index);
+        }
+    }
+
+    let mut scores : ScoreMap = HashMap::new();
+    for index in source_indexes {
+        scores.insert(index, 0);
+        calc_scores(&graph, &mut scores, &index);
+    }
+    for (key, value) in scores.iter() {
+        println!("Node {}: {}", graph.node_weight(*key).unwrap(), value);
     }
     // prints the graph in the dot format
     // which can be copied into http://viz-js.com/
