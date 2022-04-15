cac1c4893cca9550e9df8ad526cd4f66c2d81384
Roee Shoshani <roysho10@gmail.com>
2022-04-15 03:40:14 +0300

    frontend now gets laptops from api route

--- research/rating-app/pages/rate.tsx
+++ research/rating-app/pages/rate.tsx
@@ -1,51 +1,47 @@
-import React, { useState } from "react"
-import Sortable, { Laptop } from "@/components/sortable"
+import React, {useEffect, useState} from "react"
+import Sortable, {Laptop} from "@/components/sortable"
 import Button from "@/components/utils/button"
 import axios from "axios"
 
-const count = 5
-let laptops : Laptop[] = []
+const RatingPage: React.FC = () => {
+	const [laptopList, setLaptopList] = useState([] as Laptop[])
 
-for (let i = 0; i < count; i++) {
-    laptops.push({
-        id: i,
-        name: `laptop ${i}`
-    })
-}
+	useEffect(() => {
+		async function loadInitialLaptops() {
+			let initial_laptops = await axios.get('/api/initial_laptops').then(response => response.data as Laptop[])
+			setLaptopList(initial_laptops)
+		}
+		loadInitialLaptops()
+	}, [])
 
-
-
-const RatingPage : React.FC = () => {
-    const [laptopList, setLaptopList] = useState(laptops)
-
-    const postRatingAndGetNext = async () => {
-        // parse into array of ids
-        const laptopIds = laptopList.map(laptop => laptop.id)
-        // post the array of ids and manage cases
-        let nextLaptopIds = await axios.post("/api/post-rating", laptopIds)
-                                .then(response => response.data as number[])
-        
-        
-        if (nextLaptopIds) {
-            // parsing laptop id's into a laptop array.
-            // this is temporary untill our server is made
-            // before we have real laptop data
-            setLaptopList(nextLaptopIds.map(id => {
-                return {
-                    id: id,
-                    name: `laptop ${id}`
-                }
-            }))
-        }
-    }
+	const postRatingAndGetNext = async () => {
+		// parse into array of ids
+		const laptopIds = laptopList.map(laptop => laptop.id)
+		// post the array of ids and manage cases
+		let nextLaptopIds = await axios.post("/api/post-rating", laptopIds)
+			.then(response => response.data as number[])
 
 
-    return (
-        <main className="container mx-auto flex flex-col items-center gap-6 p-4">
-            <Sortable laptopList={laptopList} setLaptops={setLaptopList}/>
-            <Button onClick={postRatingAndGetNext}>Submit</Button>
-        </main>
-    )
+		if (nextLaptopIds) {
+			// parsing laptop id's into a laptop array.
+			// this is temporary untill our server is made
+			// before we have real laptop data
+			setLaptopList(nextLaptopIds.map(id => {
+				return {
+					id: id,
+					name: `laptop ${id}`
+				}
+			}))
+		}
+	}
+
+
+	return (
+		<main className="container mx-auto flex flex-col items-center gap-6 p-4">
+			<Sortable laptopList={laptopList} setLaptops={setLaptopList} />
+			<Button onClick={postRatingAndGetNext}>Submit</Button>
+		</main>
+	)
 }
 
 export default RatingPage
