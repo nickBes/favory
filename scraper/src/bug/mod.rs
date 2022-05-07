c5bf6e7d833fcab4fd10fab8fc2812b4b6422fc5
RoeeShoshani <Roee Shoshani>
2022-05-07 18:24:41 +0300

    the bug scraper now collects all the required information

--- scraper/src/bug/mod.rs
+++ scraper/src/bug/mod.rs
@@ -1,7 +1,14 @@
-use anyhow::Context;
-use scraper::{Html, Selector};
+use std::{collections::HashMap, num::ParseFloatError};
 
-use crate::{paginated_scraper::{PaginatedLaptopsScraper, PaginatedLaptopsScraperConfig}, ScrapedLaptop, ScraperClient};
+use anyhow::Context;
+use itertools::Itertools;
+use scraper::{ElementRef, Html, Selector};
+use thiserror::Error;
+
+use crate::{
+    paginated_scraper::{PaginatedLaptopsScraper, PaginatedLaptopsScraperConfig},
+    HardDrive, HardDriveKind, Memory, ParseMemoryError, ScrapedLaptop, ScraperClient, Weight,
+};
 
 #[derive(Debug, Clone)]
 pub struct BugLaptopsScraper;
@@ -21,8 +28,8 @@ impl PaginatedLaptopsScraper for BugLaptopsScraper {
             static ref PAGE_LINKS_SELECTOR: Selector = Selector::parse(".pagination > .num").unwrap();
         }
         let page_links_amount = first_page.select(&PAGE_LINKS_SELECTOR).count();
-        if page_links_amount == 0{
-            return Err(anyhow::Error::msg("couldn't find any page links"))
+        if page_links_amount == 0 {
+            return Err(anyhow::Error::msg("couldn't find any page links"));
         }
         Ok(page_links_amount)
     }
@@ -40,19 +47,156 @@ impl PaginatedLaptopsScraper for BugLaptopsScraper {
 
     fn scrape_laptop_page(&mut self, page: Html) -> anyhow::Result<ScrapedLaptop> {
         lazy_static::lazy_static! {
-            static ref LAPTOP_NAME_SELECTOR: Selector = Selector::parse("#product-page-prodict-title > div:nth-child(2)").unwrap();
+            static ref INFO_TABLE_FIRST_HALF_SELECTOR: Selector =
+                Selector::parse("#product-properties-container > div:nth-child(1)").unwrap();
+            static ref INFO_TABLE_SECOND_HALF_SELECTOR: Selector =
+                Selector::parse("#product-properties-container > div:nth-child(3)").unwrap();
+            static ref MANUFACTURER_SELECTOR: Selector = Selector::parse("#product-page-prodict-title > div.p-manufacturer > a").unwrap();
         }
+
+        const MODEL_INFO_TABLE_KEY: &str = "דגם";
+        const CPU_INFO_TABLE_KEY: &str = "מעבד";
+        const GPU_INFO_TABLE_KEY: &str = "מאיץ גרפי";
+        const MEMORY_INFO_TABLE_KEY: &str = "זכרון RAM";
+        const WEIGHT_INFO_TABLE_KEY: &str = "משקל";
+        const HARD_DRIVE_INFO_TABLE_KEY: &str = "כונן קשיח";
+        const SCREEN_SIZE_INFO_TABLE_KEY: &str = "מסך";
+
+        // parse the info table
+        let mut info_table = HashMap::new();
+        let info_table_first_half = page
+            .select(&INFO_TABLE_FIRST_HALF_SELECTOR)
+            .next()
+            .ok_or(anyhow::Error::msg("can't find info table's first half"))?;
+        parse_info_table(info_table_first_half, &mut info_table);
+
+        let info_table_second_half = page
+            .select(&INFO_TABLE_SECOND_HALF_SELECTOR)
+            .next()
+            .ok_or(anyhow::Error::msg("can't find info table's second half"))?;
+        parse_info_table(info_table_second_half, &mut info_table);
+
         Ok(ScrapedLaptop {
-            name: page
-                .select(&LAPTOP_NAME_SELECTOR)
+            model: info_table
+                .get(MODEL_INFO_TABLE_KEY)
+                .ok_or(anyhow::Error::msg("missing model in info table"))?
+                .clone(),
+            cpu: info_table
+                .get(CPU_INFO_TABLE_KEY)
+                .ok_or(anyhow::Error::msg("missing cpu in info table"))?
+                .clone(),
+            gpu: info_table
+                .get(GPU_INFO_TABLE_KEY)
+                .ok_or(anyhow::Error::msg("missing gpu in info table"))?
+                .clone(),
+            memory: info_table
+                .get(MEMORY_INFO_TABLE_KEY)
+                .ok_or(anyhow::Error::msg("missing memory in info table"))?
+                .parse()
+                .context("failed to parse memory")?,
+            weight: {
+                parse_weight(
+                    info_table
+                        .get(WEIGHT_INFO_TABLE_KEY)
+                        .ok_or(anyhow::Error::msg("missing weight in info table"))?,
+                )
+                .context("failed to parse weight")?
+            },
+            hard_drive: parse_hard_drive(
+                info_table
+                    .get(HARD_DRIVE_INFO_TABLE_KEY)
+                    .ok_or(anyhow::Error::msg("missing hard drive in info table"))?,
+            )
+            .context("failed to parse hard drive")?,
+            screen_size: info_table
+                .get(SCREEN_SIZE_INFO_TABLE_KEY)
+                .ok_or(anyhow::Error::msg("missing screen size in info table"))?
+                .parse()
+                .context("failed to parse screen size")?,
+            manufacturer: page
+                .select(&MANUFACTURER_SELECTOR)
                 .next()
-                .ok_or(anyhow::Error::msg("can't find laptop name element"))?
+                .ok_or(anyhow::Error::msg("can't find laptop manufacturer element"))?
                 .inner_html(),
         })
     }
 
-    async fn load_page(&mut self, page_number: usize, client: &ScraperClient) -> anyhow::Result<Html> {
+    async fn load_page(
+        &mut self,
+        page_number: usize,
+        client: &ScraperClient,
+    ) -> anyhow::Result<Html> {
         let url = format!("https://www.bug.co.il/laptops/?page={}", page_number);
         Ok(client.get(url).await.context("http error")?)
     }
+}
+
+/// Parses the given info table element into the `key_values` map.
+fn parse_info_table(info_table: ElementRef, key_values: &mut HashMap<String, String>) {
+    for (key_node, value_node) in info_table
+        .children()
+        .into_iter()
+        .filter_map(|node| ElementRef::wrap(node)) // only consider html elements
+        .tuples()
+    {
+        key_values.insert(key_node.inner_html(), value_node.inner_html());
+    }
+}
+
+/// Parses a weight string according to the format used in bug's website.
+fn parse_weight(weight_str: &str) -> Result<Weight, BugLaptopsScraperParseWeightError> {
+    let kilograms: f32 = weight_str.split(' ').next().unwrap().parse()?;
+    Ok(Weight::from_grams((kilograms * 1000.0) as u32))
+}
+
+#[derive(Debug, Error)]
+pub enum BugLaptopsScraperParseWeightError {
+    #[error("failed to parse quantity")]
+    FailedToParseQuantity(
+        #[from]
+        #[source]
+        ParseFloatError,
+    ),
+}
+
+/// Parses a hard drvie string according to the format used in bug's website.
+fn parse_hard_drive(
+    hard_drive_str: &str,
+) -> Result<HardDrive, BugLaptopsScraperParseHardDriveError> {
+    let (capacity_str, kind_str) = hard_drive_str
+        .rsplit_once(' ')
+        .ok_or(BugLaptopsScraperParseHardDriveError::MissingSpace)?;
+
+    // sometimes they describe the capacity as a sum of 2 capacities in different units.
+    // for example, `capacity_str` might be "1TB + 512GB"
+    let capacity: Memory = match capacity_str.split_once(" + ") {
+        Some((capacity1, capacity2)) => {
+            capacity1.parse::<Memory>()? + capacity2.parse::<Memory>()?
+        }
+        None => capacity_str.parse()?,
+    };
+
+    let kind: HardDriveKind = kind_str.parse()?;
+
+    Ok(HardDrive { capacity, kind })
+}
+
+#[derive(Debug, Error)]
+pub enum BugLaptopsScraperParseHardDriveError {
+    #[error("the hard drive string is missing a space which is supposed to separate the capacity and the hard drive kind")]
+    MissingSpace,
+
+    #[error("failed to parse hard drive capacity")]
+    FailedToParseCapacity(
+        #[from]
+        #[source]
+        ParseMemoryError,
+    ),
+
+    #[error("failed to parse hard drive kind")]
+    FailedToParseHardDriveKind(
+        #[from]
+        #[source]
+        strum::ParseError,
+    ),
 }
--- scraper/src/main.rs
+++ scraper/src/main.rs
@@ -1,12 +1,22 @@
 mod bug;
 mod paginated_scraper;
 
-use std::sync::{atomic::AtomicUsize, Arc};
+use std::{
+    fs::{File, OpenOptions},
+    io::BufWriter,
+    num::{ParseFloatError, ParseIntError},
+    str::FromStr,
+    sync::{atomic::AtomicUsize, Arc, Mutex},
+};
 
 use anyhow::Context;
 use bug::BugLaptopsScraper;
+use derive_more::Add;
 use reqwest::{header::USER_AGENT, IntoUrl};
 use scraper::Html;
+use serde::Serialize;
+use strum_macros::{Display, EnumString};
+use thiserror::Error;
 
 #[async_trait::async_trait]
 pub trait LaptopsScraper {
@@ -24,9 +34,210 @@ pub trait LaptopsProcessor: Clone + Send + 'static {
 }
 
 /// Information about a scraped laptop that each scraper should extract.
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, PartialEq, Serialize)]
 pub struct ScrapedLaptop {
-    name: String,
+    model: String,
+    manufacturer: String,
+    cpu: String,
+    gpu: String,
+    memory: Memory,
+    weight: Weight,
+    hard_drive: HardDrive,
+    screen_size: Inches,
+}
+
+const MB_PER_GB: u32 = 1000;
+const MB_PER_TB: u32 = 1000000;
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Hash, Add, Serialize)]
+pub struct Memory {
+    megabytes: u32,
+}
+impl Memory {
+    pub fn from_megabytes(megabytes: u32) -> Self {
+        Self { megabytes }
+    }
+    pub fn from_gigabytes(gigabytes: u32) -> Self {
+        Self {
+            megabytes: gigabytes * MB_PER_GB,
+        }
+    }
+    pub fn from_terabytes(terabytes: u32) -> Self {
+        Self {
+            megabytes: terabytes * MB_PER_TB,
+        }
+    }
+    pub fn megabytes(&self) -> u32 {
+        self.megabytes
+    }
+    pub fn gigabytes(&self) -> u32 {
+        self.megabytes / MB_PER_GB
+    }
+    pub fn terabytes(&self) -> u32 {
+        self.megabytes / MB_PER_TB
+    }
+}
+impl std::fmt::Display for Memory {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.megabytes >= MB_PER_TB && self.megabytes % MB_PER_TB == 0 {
+            write!(f, "{}TB", self.terabytes())
+        } else if self.megabytes >= MB_PER_GB && self.megabytes % MB_PER_GB == 0 {
+            write!(f, "{}GB", self.gigabytes())
+        } else {
+            write!(f, "{}MB", self.megabytes())
+        }
+    }
+}
+impl std::fmt::Debug for Memory {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self)
+    }
+}
+impl FromStr for Memory {
+    type Err = ParseMemoryError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let multiplier = if s.ends_with("TB") {
+            MB_PER_TB
+        } else if s.ends_with("GB") {
+            MB_PER_GB
+        } else if s.ends_with("MB") {
+            1
+        } else {
+            return Err(ParseMemoryError::MissingMemoryUnitsPostfix);
+        };
+
+        let quantity: u32 = s[..s.len() - 2].parse()?;
+
+        Ok(Self {
+            megabytes: quantity * multiplier,
+        })
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum ParseMemoryError {
+    #[error("missing memory units postfix")]
+    MissingMemoryUnitsPostfix,
+
+    #[error("failed to parse quantity")]
+    FailedToParseQuantity(
+        #[from]
+        #[source]
+        ParseIntError,
+    ),
+}
+
+const G_PER_KG: u32 = 1000;
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Hash, Add, Serialize)]
+pub struct Weight {
+    pub grams: u32,
+}
+impl Weight {
+    pub fn from_grams(grams: u32) -> Self {
+        Self { grams }
+    }
+    pub fn from_kilograms(kilograms: u32) -> Self {
+        Self {
+            grams: kilograms * G_PER_KG,
+        }
+    }
+    pub fn grams(&self) -> u32 {
+        self.grams
+    }
+}
+
+impl std::fmt::Display for Weight {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.grams >= G_PER_KG && self.grams % G_PER_KG == 0 {
+            write!(f, "{}KG", self.grams / G_PER_KG)
+        } else {
+            write!(f, "{}G", self.grams)
+        }
+    }
+}
+impl std::fmt::Debug for Weight {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self)
+    }
+}
+
+#[derive(PartialEq, Eq, Clone, Copy, Hash, Serialize)]
+pub struct HardDrive {
+    pub capacity: Memory,
+    pub kind: HardDriveKind,
+}
+
+impl std::fmt::Display for HardDrive {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {}", self.capacity, self.kind)
+    }
+}
+impl std::fmt::Debug for HardDrive {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self)
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash, Display, EnumString)]
+pub enum HardDriveKind {
+    #[strum(serialize = "SSD")]
+    Ssd,
+
+    #[strum(serialize = "eMMC")]
+    Emmc,
+}
+
+impl Serialize for HardDriveKind {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        self.to_string().serialize(serializer)
+    }
+}
+
+#[derive(PartialEq, PartialOrd, Clone, Copy, Add, Serialize)]
+pub struct Inches(pub f32);
+
+impl std::fmt::Display for Inches {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "\"{}", self.0)
+    }
+}
+
+impl std::fmt::Debug for Inches {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self)
+    }
+}
+
+impl FromStr for Inches {
+    type Err = ParseInchError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if !s.starts_with('"') {
+            return Err(ParseInchError::MissingInchSymbol);
+        }
+
+        let inches = s[1..].parse()?;
+
+        Ok(Self(inches))
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum ParseInchError {
+    #[error("missing inch symbol")]
+    MissingInchSymbol,
+
+    #[error("failed to parse quantity")]
+    FailedToParseQuantity(
+        #[from]
+        #[source]
+        ParseFloatError,
+    ),
 }
 
 /// A laptops processor which just prints laptops.
@@ -45,6 +256,21 @@ impl LaptopsProcessor for LaptopsPrinter {
             laptop_info,
             amount + 1
         );
+        Ok(())
+    }
+}
+
+/// A laptops processor which just prints laptops.
+#[derive(Debug, Clone)]
+pub struct LaptopsCollector {
+    laptops: Arc<Mutex<Vec<ScrapedLaptop>>>,
+}
+
+impl LaptopsProcessor for LaptopsCollector {
+    fn process_laptop(&mut self, laptop_info: ScrapedLaptop) -> anyhow::Result<()> {
+        let mut laptops = self.laptops.lock().unwrap();
+        laptops.push(laptop_info);
+        println!("laptops amount: {}", laptops.len());
         Ok(())
     }
 }
@@ -79,13 +305,35 @@ impl ScraperClient {
 
 #[tokio::main]
 async fn main() {
+    env_logger::init();
+    let laptops = Arc::new(Mutex::new(Vec::new()));
     BugLaptopsScraper
         .scrape_laptops(
             ScraperClient::new(),
-            LaptopsPrinter {
-                laptops_amount: Arc::new(AtomicUsize::new(0)),
+            // LaptopsPrinter {
+            //     laptops_amount: Arc::new(AtomicUsize::new(0)),
+            // },
+            LaptopsCollector {
+                laptops: Arc::clone(&laptops),
             },
         )
         .await
         .unwrap();
+
+    // save laptops to file
+    let file = BufWriter::new(
+        OpenOptions::new()
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open("laptops.json")
+            .expect("failed to open laptops.json file"),
+    );
+
+    let collected_laptops = Arc::try_unwrap(laptops)
+        .expect("some tasks are still running")
+        .into_inner()
+        .unwrap();
+
+    serde_json::to_writer_pretty(file, &collected_laptops).expect("failed to save laptops to file");
 }
