0fe6b3aa66427afdf43c6614ac0c04cf0564114f
nickBes <58567842+nickBes@users.noreply.github.com>
2022-08-04 03:37:52 +0300

    now have a dynamic vec of sorted majorities

--- research/ranked-pairs/src/main.rs
+++ research/ranked-pairs/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, vec};
 use itertools::Itertools;
 
 type LaptopId = u64;
@@ -27,67 +27,146 @@ impl PairHash {
     }
 }
 
-impl From<Pair> for PairHash {
-    fn from(pair: Pair) -> Self { PairHash::new(pair.0, pair.1) }
+impl From<&Pair> for PairHash {
+    fn from(pair: &Pair) -> Self { PairHash::new(pair.0, pair.1) }
 }
 
-struct HighIdScore(i64);
-impl HighIdScore {
-    fn new() -> Self {
-        Self(0)
-    }
-    fn high_won(&mut self) {
-        self.0 += 1;
-    }
-    fn low_won(&mut self) {
-        self.0 -= 1;
-    }
+impl From<&Majority> for PairHash {
+    fn from(majority: &Majority) -> Self { PairHash::new(majority.loser, majority.winner) }
+}
+
+impl From<Majority> for PairHash {
+    fn from(majority: Majority) -> Self { PairHash::new(majority.loser, majority.winner) }
+}
+
+type MajorityScore = u64;
+#[derive(Eq, PartialEq, Debug)]
+struct Majority {
+    winner: LaptopId,
+    loser: LaptopId,
+    score: MajorityScore
+}
+
+impl From<&Pair> for Majority {
+    fn from(pair: &Pair) -> Self { Majority { winner: pair.0, loser: pair.1, score: 1 } }
 }
 
 struct RankedPairsEngine {
-    map: HashMap<PairHash, HighIdScore>,
+    sorted_majorities: Vec<Majority>,
+    majority_indexes: HashMap<PairHash, usize>
 }
+
 impl RankedPairsEngine {
     pub fn new() -> Self {
         Self {
-            map: HashMap::new()
+            // map: HashMap::new(),
+            sorted_majorities: Vec::new(),
+            majority_indexes: HashMap::new()
         }
     }
     // adds pairs from each combinations that describes
     // a preference of the previous item over the later
     pub fn vote(&mut self, vote: &Vote) {
         for (winner, losser) in vote.iter().tuple_combinations() {
-            self.add_pair(Pair(*winner, *losser))
+            self.add_pair(&Pair(*winner, *losser))
         }
     }
-    fn add_pair(&mut self, pair: Pair) {
-        // if the winner id is the lower one
-        if pair.0 < pair.1 {
-            self.map
-                .entry(PairHash { _low: pair.0, _high: pair.1 })
-                .or_insert_with(HighIdScore::new)
-                .low_won();
+
+    pub fn print_majorities(&self) {
+        println!("{:?}", self.sorted_majorities);
+    }
+
+    fn increase_majority_score(&mut self, index: usize) {
+        self.sorted_majorities[index].score += 1;
+        let current_majority = &self.sorted_majorities[index];
+
+        // look for the farthest closest that its score is smaller by one
+        // and then swap
+        let swap_index = self.sorted_majorities[0..index]
+                                        .iter()
+                                        .rposition(|m2| m2.score < current_majority.score);
+
+        if let Some(i) = swap_index {
+            // adding one because the index we found is for the bigger majority
+            let to_swap_majority = &self.sorted_majorities[i];
+
+            // update the majority indexes map
+            self.majority_indexes.insert(to_swap_majority.into(), index);
+            self.majority_indexes.insert(current_majority.into(), i);
+
+            self.sorted_majorities.swap(i, index);
+        }
+    }
+
+    fn decrease_majority_score(&mut self, index: usize) {
+        self.sorted_majorities[index].score -= 1;
+        let current_majority = &self.sorted_majorities[index];
+
+        if current_majority.score == 0 {
+            // then the majority score is below the minimum
+            self.majority_indexes.remove(&self.sorted_majorities
+                                                                .swap_remove(index)
+                                                                .into());
+            
         } else {
-            // the winner id is the higher one
-            self.map
-                .entry(PairHash { _low: pair.1, _high: pair.0 })
-                .or_insert_with(HighIdScore::new)
-                .high_won();
+            // look for the closest majority which score is the same
+            // and then swap
+            let swap_index = self.sorted_majorities[index + 1..self.sorted_majorities.len()]
+                                        .iter()
+                                        .position(|m2| m2.score == current_majority.score);
+            
+            if let Some(i) = swap_index {
+                let to_swap_majority = &self.sorted_majorities[i];
+
+                // update the majority indexes map
+                // update the majority indexes map
+                self.majority_indexes.insert(to_swap_majority.into(), index);
+                self.majority_indexes.insert(current_majority.into(), i);
+
+                self.sorted_majorities.swap(i, index);
+            }
+        }
+    }
+
+    fn add_pair(&mut self, pair: &Pair) {
+        let hash: PairHash = pair.into();
+
+        match self.majority_indexes.get(&hash) {
+            Some(&index) => {
+                let current_majority = &self.sorted_majorities[index];
+                if current_majority.winner == pair.0 {
+                    // then the vec had an existing majority
+                    // and we can increase the score
+                    self.increase_majority_score(index);
+                } else {
+                    // the vec had an opposite majority
+                    self.decrease_majority_score(index);
+                }
+            },
+            _ => { // adding a new pair down to the sorted majorities, as it has the minimal value
+                self.majority_indexes.insert(hash, self.sorted_majorities.len());
+                self.sorted_majorities.push(pair.into());
+            }
         }
     }
 }
 
 fn main() {
-    let laptops = vec!['a', 'b', 'c', 'd'];
     let mut engine = RankedPairsEngine::new();
     let votings: Vec<Vote> = vec![
-        vec![0, 1, 2, 3],
-        vec![0, 3, 4, 1],
         vec![0, 1],
-        vec![4, 1]
+        vec![0, 1],
+        vec![4, 1],
+        vec![4, 1],
+        vec![1, 2]
     ];
     for vote in votings {
         engine.vote(&vote);
     }
+    engine.print_majorities();
+
+    engine.vote(&vec![1,0]);
+
+    engine.print_majorities();
     println!("Hello, world!");
 }
