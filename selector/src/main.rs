902726fa3333259077eb14484ed2eda35220e058
nickBes <58567842+nickBes@users.noreply.github.com>
2023-09-26 23:13:00 +0300

    now using priority queue instead of manually sorting top laptops

--- selector/src/main.rs
+++ selector/src/main.rs
@@ -18,11 +18,17 @@ fn main() {
     // initialize logger
     if cfg!(debug_assertions) {
         // in debug mode print logs to stdout
-        SimpleLogger::init(LevelFilter::max(), Config::default()).expect("failed to initialize logger");
-    } else{
+        SimpleLogger::init(LevelFilter::max(), Config::default())
+            .expect("failed to initialize logger");
+    } else {
         // in release mode print logs to file
-        let config_file = OpenOptions::new().write(true).create(true).open("log").expect("failed to open log file");
-        WriteLogger::init(LevelFilter::Warn, Config::default(), config_file).expect("failed to initialize logger");
+        let config_file = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open("log")
+            .expect("failed to open log file");
+        WriteLogger::init(LevelFilter::max(), Config::default(), config_file)
+            .expect("failed to initialize logger");
     }
 
     let db_connection = SelectorDBConnection(db_access::get_db_connection());
